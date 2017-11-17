cfa94097666fed5653b37597b472c8eedada7247
cryptoworldfan <30868611+cryptoworldfan@users.noreply.github.com>
2017-11-18 01:35:21 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+<meta charset="utf-8">
+<title>Calculator</title>
+</head>
+<body>
+<form method="post" attribute="post" action="coordtp2.php">
+<p>X1:<br/>
+<input type="text" id="x1" name="x1"></p>
+<p>Y1:<br/>
+<input type="text" id="y1" name="y1"></p>
+<p>X2:<br/>
+<input type="text" id="x2" name="x2"></p>
+<p>Y2:<br/>
+<input type="text" id="y2" name="y2"></p>
+<p></p>
+<button type="submit" name="answer" id="answer" value="answer">Calculate</button>
+</form>
+</body>
+</html>
