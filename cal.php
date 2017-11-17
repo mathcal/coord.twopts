4dd7e498e24c3cfea8687700138156064150ce59
cryptoworldfan <30868611+cryptoworldfan@users.noreply.github.com>
2017-11-18 01:36:00 +0330

    Create cal.php

---
+++ cal.php
@@ -0,0 +1,15 @@
+<?PHP
+$x1 = $_POST['x1'];
+$y1 = $_POST['y1'];
+$x2 = $_POST['x2'];
+$y2 = $_POST['y2'];
+$onea = $y2 - $y1;
+$twoa = $x2 - $x1;
+$slope = $onea / $twoa;
+$slopex = "". $slope ."X";
+$xnew = -$x1;
+$slopex1 = $slope * $xnew;
+$sum = $slopex1 + $y1;
+echo "Slope: $slope";
+echo "Equation of line: Y = $slopex + $sum";
+?>
