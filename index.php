5b0a885c1e317b77598f3e7ca5489ed345bcf894
aneelaehsan <42001486+aneelaehsan@users.noreply.github.com>
2019-10-27 21:01:32 -0400

    Update index.php
    Added more instructions and formatted the file

--- index.php
+++ index.php
@@ -3,17 +3,19 @@
 <title>Calculator</title>
 </head>
 <body>
-<form method="post" attribute="post" action="cal.php">
-<p>X1:<br/>
-<input type="text" id="x1" name="x1"></p>
-<p>Y1:<br/>
-<input type="text" id="y1" name="y1"></p>
-<p>X2:<br/>
-<input type="text" id="x2" name="x2"></p>
-<p>Y2:<br/>
-<input type="text" id="y2" name="y2"></p>
-<p></p>
-<button type="submit" name="answer" id="answer" value="answer">Calculate</button>
-</form>
+  <form method="post" attribute="post" action="cal.php">
+    <p>Enter the coordinates of the first point:</p>
+    <p>X1:<br/>
+    <input type="text" id="x1" name="x1"></p>
+    <p>Y1:<br/>
+    <input type="text" id="y1" name="y1"></p>
+    <p>Enter the coordinated of the second point:</p>
+    <p>X2:<br/>
+    <input type="text" id="x2" name="x2"></p>
+    <p>Y2:<br/>
+    <input type="text" id="y2" name="y2"></p>
+    <p></p>
+    <button type="submit" name="answer" id="answer" value="answer">Calculate</button>
+  </form>
 </body>
 </html>
